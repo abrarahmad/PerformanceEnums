d6070287f0265dd1612a6e0d84fff59b767f50d7
Abrar Ahmad Ansari <abrarahmad.ansari501@gmail.com>
2022-06-14 01:11:15 +0530

    Add project files.

---
+++ Benchmarks.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace PerformanceEnums
+{
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        [Benchmark]
+        public string EnumToString()=> Color.LightGreen.ToString();
+        
+        [Benchmark]
+        public string EnumToStringFast() => Color.LightGreen.ToStringFast();
+
+        [Benchmark]
+        public bool EnumIsDefined() => Enum.IsDefined(typeof(Color), (Color)69);
+
+        [Benchmark]
+        public bool EnumIsDefinedFast() => ColorExtensions.IsDefined((Color)69);
+
+        [Benchmark]
+        public (bool, Color) EnumTryParse()
+        {
+            var couldParse = Enum.TryParse("LightGreen",false, out Color value);
+                return (couldParse, value);
+        }
+
+        [Benchmark]
+        public (bool, Color) EnumTryParseFast()
+        {
+            var couldParse = ColorExtensions.TryParse("LightGreen", false, out Color value);
+            return (couldParse, value);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using BenchmarkDotNet.Running;
+using PerformanceEnums;
+
+
+//https://github.com/andrewlock/NetEscapades.EnumGenerators
+
+BenchmarkRunner.Run<Benchmarks>();
+
+//var color = Color.LightGreen;
+//Console.WriteLine(color.ToString());
+//var matched = Enum.TryParse("LightGreen", out Color theColor);
+
+//var isDefined = Enum.IsDefined(typeof(Color), (Color)69);
